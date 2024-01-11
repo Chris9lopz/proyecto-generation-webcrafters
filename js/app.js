2694ff804ee9780cefee621731cc9d3e07b7078c
LauraBaronBarna <laurabaronbarna@gmail.com>
2024-01-11 16:23:23 -0500

    lenguajes implementados

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/* ----- LENGUAJES IMPLEMENTADOS (con la página externa para animación)-----*/
+
+let swiperlenguajes = new Swiper(".lenguajes__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+ /* FIN LENGUAJES IMPLEMENTADOS */
