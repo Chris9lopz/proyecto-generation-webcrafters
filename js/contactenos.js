749be23d4d08a9608dd28ead014a705a096156a9
naomihyuga <paolarodriguezcastro10@gmail.com>
2024-01-21 23:29:15 +0100

    revision de validacion

--- js/contactenos.js
+++ js/contactenos.js
@@ -1,98 +1,134 @@
-const nombre = document.getElementById("nombre");
-const correo= document.getElementById("correo");
-const celular = document.getElementById("celular");
-const form = document.getElementById("miFormulario");
-const parrafo = document.getElementById("warnings");
+//expresiones regulares para validacion
+const expresionNombre = /^[a-zA-ZÀ-ÿ\s]+$/;
+const expNombre = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+const expresionCorreo = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+const expresionTelefono =/^\d{3}-\d{3}-\d{4}$/;
+//const expresionMovil = /^(?:(?:\+|00)34?|0)?[6789]\d{8}$/;
 
-form.addEventListener("submit", e =>{
+
+//validacion de expresiones regulares
+
+//funcion validacion nombre
+  const validarNombre = (nombre) => {
+    return expresionNombre.test(nombre);
+  };
+
+//funcion validacion correo
+  const validarCorreo = (correo) => {
+    return expresionCorreo.test(correo);
+  };
+
+  //funcion validacion telefono
+  
+  const validarTelefono = (telefono) => {
+    return expresionTelefono.test(telefono);
+  };
+  
+  let form = document.getElementById('miFormulario');
+
+  form.addEventListener('submit', function(e){
     e.preventDefault();
+    
+    //variables
+    
+    let nombre = document.getElementById('nombre').value;
+    let correo = document.getElementById('correo').value;
+    let telefono = document.getElementById('celular').value;
+    let mensaje = document.getElementById('mensaje').value;
+   
 
-validateInputs();
+    //variables de mesnajes de error
+    
+      const nombreError = document.getElementById('nameWarning');
+      const correoError = document.getElementById('emailWarning');
+      const telefonoError = document.getElementById('phoneWarning');
+      const mensajeError = document.getElementById('messageWarning');
 
-});
-
-const SetError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success')
-
-}
-
-const setSuccess = element => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error')
-}
-
-const isValidEmail = email => {
-    const  regexEmail =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    return regexEmail.test(String(email).toLowerCase())
-}
-
-const isValidMovil = movil =>{
-    const regexMovil =  /^\d{7,14}$/
-    return regexMovil.test(movil.value);
-}
-const validateInputs = () => {
-        const nombreValue = nombre.value.trim();
-        const emailValue = correo.value.trim();
-        const movilValue = celular.value.trim();
+    
+    //validar nombre
+    let nombreValido = false;
+    if(nombre == "" || nombre == 0 ){
         
-        if(nombreValue === '' || emailValue === '' ||  movilValue === ''){
-            if(nombreValue === ''){
-                SetError(nombre, "El nombre es requerido");
-            }else{
-                setSuccess(nombre)
-            }
-    
-            if(emailValue === ''){
-                SetError(correo, "El correo es requerido");
-            }else if(!isValidEmail(emailValue)){
-                SetError(correo, "incluye el @  o .com para validar el correo");
-            }else{
-                setSuccess(correo);
-            }
-    
-            if(movilValue === ''){
-                SetError(celular, "El numero movil es requerido");
-            }else{
-                setSuccess(celular);
-            }
-
-        }else{
-            if(nombreValue === ''){
-                SetError(nombre, "El nombre es requerido");
-            }else{
-                setSuccess(nombre)
-            }
-    
-            if(emailValue === ''){
-                SetError(correo, "El correo es requerido");
-            }else if(!isValidEmail(emailValue)){
-                SetError(correo, "incluye el @  o .com para validar el correo");
-            }else{
-                setSuccess(correo);
-            }
-    
-            if(movilValue === ''){
-                SetError(celular, "El numero movil es requerido");
-            }else{
-                setSuccess(celular);
-            }
-        }
-
-       
-
+        nombreError.classList.add('error');
+        nombreError.innerText = 'Por favor, ingresa tu nombre.';
+        document.getElementById("nombre").style.border= '1px solid red';
         
+    }
+    else if(!validarNombre(nombre)){
+        nombreError.innerText = 'Tiene que ser solamente texto con minusculas o mayusculas';
+        
+    }else{
+        document.getElementById("nombre").style.border= 'none';
+        nombreError.innerText = '';
+         nombreValido = true;
+    }
 
 
-}
+    //validar correo
+    correoValido = false;
+    if(correo == "" || correo == 0){
+         correoError.classList.add( 'error');
+         correoError.innerText = 'Por favor, ingresa tu número correo.';
+         document.getElementById("correo").style.border = " 1px solid red";
+         
+    
+    }
+    else if(!validarCorreo(correo)){
+        
+        correoError.innerText =  `incluye un signo "@" en la direccion de correo. La direcion de correo:" ${correo}" no es un correo. ` ;
+        
+    }else{
+        document.getElementById("correo").style.border = " none";
+        correoError.innerText = '';
+         correoValido = true;
+    }
+
+    //validar telefono
+    let telefonoValido = false;
+    if(telefono == "" || telefono == 0){
+        
+         telefonoError.classList.add('error');
+         telefonoError.innerText = 'Por favor, ingresa tu numero telefono.';
+         document.getElementById("celular").style.border = " 1px solid red";
+         
+      
+    
+        
+    }else if(!validarTelefono(telefono) &&  telefono <= 5){
+        
+        telefonoError.innerText = 'Tiene que ser solamente numeros y tiene que tener mas de 5 numeros';
+        
+    }else{
+        document.getElementById("celular").style.border = " none";
+        telefonoError.innerText = '';
+         telefonoValido = true;
+    }
+
+    //validar textarea mensaje
+    let mensajeValido = false;
+    if(mensaje == "" || mensaje == 0 ){
+        mensajeError.classList.add('error');
+        mensajeError.innerText = 'Por favor, ingresa un comnetario.';
+        document.getElementById("mensaje").style.border= '1px solid red';
+        
+    }
+    else{
+        document.getElementById("mensaje").style.border= 'none';
+        mensajeError.innerText = '';
+         mensajeValido = true;
+    }
+
+    if(mensajeValido && telefonoValido && correoValido && nombreValido){
+        Swal.fire({
+            icon: "success",
+            title: "Mensaje enviado con exito",
+            showConfirmButton: false,
+            timer: 1000
+      });
+          form.reset();
+          
+    }
+    
 
 
-
+  });
