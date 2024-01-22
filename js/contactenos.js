c5869a0df8a2f3d14d82c5ee3577f604035895ea
LizethPulidoD <ldpulidod@gmail.com>
2024-01-21 21:48:04 -0500

    Limpieza de codigo realizada

--- js/contactenos.js
+++ js/contactenos.js
@@ -24,99 +24,99 @@ const mensajeError = document.getElementById('messageWarning');
 //validacion
 
 //validacion nombre
-function validarNombre() {
-  //validar nombre  vacio
-  let nombreAValidar = nombre.value;
-  if (nombreAValidar == "" || nombreAValidar == 0) {
-    nombreError.classList.add('error');
-    nombreError.innerText = 'Por favor, ingresa tu nombre.';
-    document.getElementById("name").style.border = '1px solid #951018';
-    return false;
-  } else if (!expresionNombre.test(nombreAValidar)) {
-    nombreError.classList.add('error');
-    nombreError.innerText = 'Tiene que ser solamente texto con minusculas o mayusculas';
-    document.getElementById("name").style.border = '1px solid #951018';
-    return false;
-  } else {
-    document.getElementById("name").style.border = 'none';
-    nombreError.innerText = '';
-    return true;
-  }
+const validarNombre = () => {
+    //validar nombre  vacio
+    let nombreAValidar = nombre.value;
+    if (nombreAValidar == "" || nombreAValidar == 0) {
+        nombreError.classList.add('error');
+        nombreError.innerText = 'Por favor, ingresa tu nombre.';
+        document.getElementById("name").style.border = '1px solid #951018';
+        return false;
+    } else if (!expresionNombre.test(nombreAValidar)) {
+        nombreError.classList.add('error');
+        nombreError.innerText = 'Tiene que ser solamente texto con minusculas o mayusculas';
+        document.getElementById("name").style.border = '1px solid #951018';
+        return false;
+    } else {
+        document.getElementById("name").style.border = 'none';
+        nombreError.innerText = '';
+        return true;
+    }
 };
 
 //funcion validacion correo
-function validarCorreo() {
-  let correoAValidar = correo.value;
-  //validar correo vacio
-  if (correoAValidar == "" || correoAValidar == 0) {
-    correoError.classList.add('error');
-    correoError.innerText = 'Por favor, ingresa tu número correo.';
-    document.getElementById("email").style.border = " 1px solid #951018";
-    return false;
-  } else if (!expresionCorreo.test(correoAValidar)) {
-    correoError.classList.add('error');
-    correoError.innerText = `incluye un signo "@" en la direccion de correo. La direcion de correo:" ${correo}" no es un correo. `;
-    document.getElementById("email").style.border = " 1px solid #951018";
-    return false;
-  } else {
-    document.getElementById("email").style.border = "none";
-    correoError.innerText = '';
-    return true;
-  }
+const validarCorreo = () => {
+    let correoAValidar = correo.value;
+    //validar correo vacio
+    if (correoAValidar == "" || correoAValidar == 0) {
+        correoError.classList.add('error');
+        correoError.innerText = 'Por favor, ingresa tu número correo.';
+        document.getElementById("email").style.border = " 1px solid #951018";
+        return false;
+    } else if (!expresionCorreo.test(correoAValidar)) {
+        correoError.classList.add('error');
+        correoError.innerText = `incluye un signo "@" en la direccion de correo. La direcion de correo:" ${correo}" no es un correo. `;
+        document.getElementById("email").style.border = " 1px solid #951018";
+        return false;
+    } else {
+        document.getElementById("email").style.border = "none";
+        correoError.innerText = '';
+        return true;
+    }
 };
 
 //funcion validacion telefono
 
-function validarTelefono() {
-  let telefonoAValidar = telefono.value;
-  //validar telefono vacio
-  if (telefonoAValidar == "" || telefonoAValidar == 0) {
-    telefonoError.classList.add('error');
-    telefonoError.innerText = 'Por favor, ingresa tu numero telefono.';
-    document.getElementById("celular").style.border = " 1px solid #951018";
-    return false;
-  } else if (!expresionTelefono.test(telefonoAValidar)) {
-    telefonoError.classList.add('error');
-    telefonoError.innerText = 'Tiene que ser solamente numeros y tiene que tener mas de 10 numeros';
-    document.getElementById("celular").style.border = " 1px solid #951018";
-    return false;
-  } else {
-    document.getElementById("celular").style.border = " none";
-    telefonoError.innerText = '';
-    return true;
-  }
+const validarTelefono = () => {
+    let telefonoAValidar = telefono.value;
+    //validar telefono vacio
+    if (telefonoAValidar == "" || telefonoAValidar == 0) {
+        telefonoError.classList.add('error');
+        telefonoError.innerText = 'Por favor, ingresa tu numero telefono.';
+        document.getElementById("celular").style.border = " 1px solid #951018";
+        return false;
+    } else if (!expresionTelefono.test(telefonoAValidar)) {
+        telefonoError.classList.add('error');
+        telefonoError.innerText = 'Tiene que ser solamente numeros y tiene que tener mas de 10 numeros';
+        document.getElementById("celular").style.border = " 1px solid #951018";
+        return false;
+    } else {
+        document.getElementById("celular").style.border = " none";
+        telefonoError.innerText = '';
+        return true;
+    }
 };
 
-function validarMensaje() {
-  let textoAValidar = mensaje.value;
-  //validar textarea mensaje
-  if (textoAValidar == "" || textoAValidar == 0) {
-    mensajeError.classList.add('error');
-    mensajeError.innerText = 'Por favor, ingresa un comentario.';
-    document.getElementById("mensaje").style.border = '1px solid #951018';
-    return false;
-  }
-  else {
-    document.getElementById("mensaje").style.border = 'none';
-    mensajeError.innerText = '';
-    return true;
-  }
+const validarMensaje = () => {
+    let textoAValidar = mensaje.value;
+    //validar textarea mensaje
+    if (textoAValidar == "" || textoAValidar == 0) {
+        mensajeError.classList.add('error');
+        mensajeError.innerText = 'Por favor, ingresa un comentario.';
+        document.getElementById("mensaje").style.border = '1px solid #951018';
+        return false;
+    }
+    else {
+        document.getElementById("mensaje").style.border = 'none';
+        mensajeError.innerText = '';
+        return true;
+    }
 }
 
 
 form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  let tieneNombre = validarNombre();
-  let tieneCorreo = validarCorreo();
-  let tieneTelefono = validarTelefono();
-  let tieneMensaje = validarMensaje();
-  if (tieneNombre && tieneCorreo && tieneTelefono  && tieneMensaje ) {
-    form.submit();
-    Swal.fire({
-      icon: "success",
-      title: "Mensaje enviado con exito",
-      showConfirmButton: false,
-      timer: 1000
-    });
-  }
+    e.preventDefault();
+    let nombreValido = validarNombre();
+    let correoValido = validarCorreo();
+    let telefonoValido = validarTelefono();
+    let mensajeValido = validarMensaje();
+    if (nombreValido && correoValido && telefonoValido && mensajeValido) {
+        form.submit();
+        Swal.fire({
+            icon: "success",
+            title: "Mensaje enviado con exito",
+            showConfirmButton: false,
+            timer: 1000
+        });
+    }
 });
