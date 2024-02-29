beab30a96468446dc4c182f05912839bb5434e5f
Christian Lopez <80645989+Chris9lopz@users.noreply.github.com>
2024-02-28 20:29:49 -0500

    fix: se corrige el logueo

--- js/index.js
+++ js/index.js
@@ -49,3 +49,13 @@ var multipleCardCarousel = document.querySelector(
     $(multipleCardCarousel).addClass("slide");
   }
 
+  function actualizarEstado() {
+    if (localStorage.getItem('logueado') == 1){
+      let iniciarS = document.querySelector('.dropdown-item');
+      iniciarS.innerText = 'Cerrar Sesión';
+    } else {
+      console.log("No se ha recuperado el elemento");
+    }
+  }
+  // Llamado de función
+actualizarEstado();
