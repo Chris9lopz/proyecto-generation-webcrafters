d209852431eaa8589b2afbc54018d9ca5efa0f1d
naomihyuga <paolarodriguezcastro10@gmail.com>
2024-03-06 10:11:06 +0100

    Agregar Carrito Actualizacion

--- js/productos.js
+++ js/productos.js
@@ -46,64 +46,8 @@ class Productos {
 }
 
 
-/*************Productos************** */
 
-const filterElements = document.querySelector(".lst-filtroProductos");
-const plantillaItems = document.querySelectorAll(".card");
-filterElements.addEventListener("click", (e) => {
-  if (e.target.classList.contains("filter-item")) {
-    filterElements.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
-
-    const filterValue = e.target.getAttribute("data-filter");
-    plantillaItems.forEach(item => {
-      if (item.classList.contains(filterValue) || filterValue == "all") {
-        item.classList.remove("hide");
-        item.classList.add("show");
-      } else {
-        item.classList.remove("show");
-        item.classList.add("hide");
-      }
-    })
-  }
-
-
-  // Obtener elementos necesarios
-  const dropdownButton = document.getElementById('dropdownMenuButton1');
-  const dropdownItems = document.querySelectorAll('.filter-item');
-
-  // Agregar evento de clic a cada elemento de la lista
-  dropdownItems.forEach(function (item) {
-    item.addEventListener('click', function () {
-      // Obtener el texto del elemento clicado
-      let selectedText = item.innerText;
-
-      // Actualizar el texto del botón con el texto seleccionado
-      dropdownButton.innerText = selectedText;
-
-
-
-    });
-  });
-
-  /************************************ */
-
-  if (!e.target.matches('.lst-filtro')) {
-    let dropdowns = document.getElementsByClassName("dropdown-menu");
-
-    for (i = 0; i < dropdowns.length; i++) {
-      let openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('menus')) {
-        openDropdown.classList.add('menus')
-        openDropdown.classList.remove('menus');
-      } else {
-        openDropdown.classList.remove('menus');
-        openDropdown.classList.add('menus')
-
-      }
-    }
-  }
-});
+/** 
 
 // Crear una instancias de la clase Productos
 let producto1 = new Productos();
@@ -201,3 +145,140 @@ function almacenarProducto(producto, instanciaProducto) {
 for (let i = 0; i < productos.length; i++) {
   almacenarProducto(productos[i], instanciaProducto[i]);
 }
+
+*/
+
+
+
+
+
+////*********** productos********** *////
+
+
+const shopContent = document.getElementById("row-galeria")
+const verCarrito = document.getElementById("verCarrito")
+const modalContainer = document.getElementById("ContenedoragregarPlantilla")
+const cantidadCarrito = document.getElementById("cantidadCarrito")
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+const getProducts = async () =>{
+    const response = await fetch("data.json");
+    const data = await response.json();
+
+}
+
+plantillas.forEach((product) => {
+    modalContainer.innerHtml = " ";
+    let content = document.createElement('div');
+    content.classList = `productos card product  ${product.filtro}`;
+    content.innerHTML = `
+    <img src=${product.img} class="card-img-top carrito-img-top"> 
+    <div class="card-body">
+            <h5>${product.nombre}</h5>
+            <p class="card-text">${product.categoria}</p>
+            <p class="card-text">$${product.precio}</p>
+          </div>
+    `
+    shopContent.append(content);
+   
+    let comprar = document.createElement("button");
+    comprar.innerText = "agregar carrito"
+    comprar.className = "comprar"
+    comprar.innerHTML = `
+    <div class="card-body carrito">
+    <a id="prod-1"><i class="fas fa-shopping-cart"></i> </a>
+    <p>Agregar carrito</p>
+   </div>
+    `
+
+    content.append(comprar)
+
+    comprar.addEventListener("click", ()=>{
+        carrito.push({
+            id: product.id,
+            img : product.img,
+            nombre: product.nombre,
+            precio: product.precio
+        });
+        console.log(carrito);
+        carritoCounter();
+         saveLocal();
+    })
+    
+});
+//set item
+const saveLocal = () => {
+    localStorage.setItem('carrito',JSON.stringify(carrito) );
+}
+
+
+getProducts()
+
+
+/*filtro productos* */
+
+const filterElements = document.querySelector(".lst-filtroProductos");
+const plantillaItems = document.querySelectorAll(".product");
+
+filterElements.addEventListener("click", (e) => {
+  e.preventDefault()
+  if (e.target.classList.contains("filter-item")) {
+    filterElements.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    const filterValue = e.target.getAttribute("data-filter");
+    
+    plantillaItems.forEach(item => {
+      console.log(item);
+      if (item.classList.contains(filterValue) || filterValue == "all") {
+        item.classList.remove("hide");
+        item.classList.add("show");
+      } else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+      }
+    })
+  }
+
+
+  // Obtener elementos necesarios
+  const dropdownButton = document.getElementById('dropdownMenuButton1');
+  const dropdownItems = document.querySelectorAll('.filter-item');
+
+  // Agregar evento de clic a cada elemento de la lista
+  dropdownItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      // Obtener el texto del elemento clicado
+      let selectedText = item.innerText;
+      // Actualizar el texto del botón con el texto seleccionado
+      dropdownButton.innerText = selectedText;
+    });
+  });
+
+  /************************************ */
+
+  if (!e.target.matches('.lst-filtro')) {
+    let dropdowns = document.getElementsByClassName("dropdown-menu");
+
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if(openDropdown.classList.contains('menus')) {
+        openDropdown.classList.add('menus')
+        openDropdown.classList.remove('menus');
+      } else {
+        openDropdown.classList.remove('menus');
+        openDropdown.classList.add('menus')
+
+      }
+    }
+  }
+});
+
+
+
+
+
+
+
+
